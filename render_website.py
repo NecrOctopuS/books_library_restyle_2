cbb17037439ac1d47c3bc268f92462d2b9a03e2c
NecrOctopuS <53893624+NecrOctopuS@users.noreply.github.com>
2020-04-18 21:20:55 +0300

    Add if main

--- render_website.py
+++ render_website.py
@@ -7,7 +7,7 @@ folder = 'pages'
 BOOKS_PER_PAGE = 6
 
 
-def on_reload():
+def on_reload(env, chunks):
     template = env.get_template('template.html')
     os.makedirs(folder, exist_ok=True)
     max_page_number = len(chunks)
@@ -19,17 +19,22 @@ def on_reload():
             file.write(rendered_page)
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-with open("book_informations.json", "r", encoding="utf8") as my_file:
-    book_informations = my_file.read()
-books = json.loads(book_informations)
-chunks = [books[x:x + BOOKS_PER_PAGE] for x in range(0, len(books), BOOKS_PER_PAGE)]
+    with open("book_informations.json", "r", encoding="utf8") as my_file:
+        book_informations = my_file.read()
+    books = json.loads(book_informations)
+    chunks = [books[x:x + BOOKS_PER_PAGE] for x in range(0, len(books), BOOKS_PER_PAGE)]
 
-on_reload()
-server = Server()
-server.watch('template.html', on_reload)
-server.serve(root='.')
+    on_reload(env, chunks)
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.')
+
+
+if __name__ == '__main__':
+    main()
